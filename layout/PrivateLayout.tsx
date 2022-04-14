5ead0484d0b6ac17f49e52a37d9d3fae3f0a5d4f
Brandon Ortiz <ortiz0505@gmail.com>
2022-04-14 00:26:09 -0500

    v0.9

--- layout/PrivateLayout.tsx
+++ layout/PrivateLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { signIn, useSession } from 'next-auth/react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import Loading from '@components/Loading';
+import Loading from 'components/Loading';
 
 const PrivateLayout = ({ children }: any) => {
   const { data: session, status } = useSession();
