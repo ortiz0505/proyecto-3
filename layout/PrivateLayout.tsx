6b720d34189b66f68e07a89a0d38239be6d782b1
Brandon Ortiz <ortiz0505@gmail.com>
2022-04-14 00:05:32 -0500

    fix imports errors

--- layout/PrivateLayout.tsx
+++ layout/PrivateLayout.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { signIn, useSession } from 'next-auth/react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import NavBar from 'components/NavBar';
-import Loading from 'components/Loading';
+import NavBar from '@components/NavBar';
+import Loading from '@components/Loading';
 
 const PrivateLayout = ({ children }: any) => {
   const { data: session, status } = useSession();
